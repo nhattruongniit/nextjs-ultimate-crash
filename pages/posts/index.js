62e6a0d098e049ac9803c89973361d617db696c3
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-08-22 19:05:01 +0700

    add file

---
+++ pages/posts/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../../components/Layout";
+
+function Posts() {
+  return (
+    <Layout>
+      <h1>this is Posts</h1>
+    </Layout>
+  )
+}
+
+export default Posts
